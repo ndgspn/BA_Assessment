8d92286757126b079dfebbeda4bb936f0d0be3da
ndgspn <nandangsopyan08@gmail.com>
2019-02-10 23:11:33 +0700

    change response

--- binar_api/app/controllers/users_controller.rb
+++ binar_api/app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    return render json: {
-      status: :OK,
-      result: user,
-      errors: user.errors.full_messages
-    }, status: :OK if user.save
-
+    return render json: { status: :OK, result: user, errors: user.errors.full_messages }, status: :OK if user.save
     render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
   end
 
