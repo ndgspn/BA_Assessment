42b606bbd826f64ae75f8c0846e481896d511625
ndgspn <nandangsopyan08@gmail.com>
2019-02-15 07:03:24 +0700

    add before_action for product_id, DRY

--- binar_api/app/controllers/api/v1/api_controller.rb
+++ binar_api/app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,19 @@
 module Api::V1
   class ApiController < ApplicationController
+    def response_success
+      render json: { status: :OK, result: @product }
+    end
+
+    def response_deleted
+      render json: { status: :OK, result: { message: "product with id: #{params[:id]} successfully deleted" } }
+    end
+
+    def response_error
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def product_not_found
+      render json: { errors: "Couldn't find product with id: #{params[:id]}" }
+    end
   end
 end
--- binar_api/app/controllers/api/v1/products_controller.rb
+++ binar_api/app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 module Api::V1
   class ProductsController < ApiController
     before_action :authorize_request
+    before_action :product_id, only: [:show, :update, :destroy]
     rescue_from ActiveRecord::RecordNotFound, with: :product_not_found
 
     def index
@@ -9,7 +10,6 @@ module Api::V1
     end
 
     def show
-      @product = Product.product_id(params)
       return response_success if @product
     end
 
@@ -20,13 +20,11 @@ module Api::V1
     end
 
     def update
-      @product = Product.product_id(params)
       return response_success if @product.update(product_params)
       response_error
     end
 
     def destroy
-      @product = Product.product_id(params)
       return response_deleted if @product.destroy
     end
 
@@ -35,20 +33,8 @@ module Api::V1
       params.permit(:name, :price, :imageurl)
     end
 
-    def response_success
-      render json: { status: :OK, result: @product }
-    end
-
-    def response_deleted
-      render json: { status: :OK, result: { message: "product with id: #{params[:id]} successfully deleted" } }
-    end
-
-    def response_error
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
-    end
-
-    def product_not_found
-      render json: { errors: "Couldn't find product with id: #{params[:id]}" }
+    def product_id
+      @product = Product.product_id(params)
     end
   end
 end
